8d4d9c851acc1236d664be17d0bd8b5992a21625
PasinduTY <pasindu.t.y.s@gmail.com>
2026-01-19 12:10:13 +0530

    initialize ASP.NET Core Web API project

---
+++ backend/PurchaseOrderAPI/PurchaseOrderAPI/Models/PurchaseOrder.cs
@@ -0,0 +1,13 @@
+﻿namespace PurchaseOrderAPI.Models
+{
+    public class PurchaseOrder
+    {
+        public int Id { get; set; }
+        public string PoNumber { get; set; }  // Unique
+        public string Description { get; set; }
+        public string SupplierName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } // Draft/Approved/Shipped/Completed/Cancelled
+    }
+}
