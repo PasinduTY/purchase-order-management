96fd15453ae66d55e7fd4aa4c678f81fa6c0671f
PasinduTY <pasindu.t.y.s@gmail.com>
2026-01-19 12:38:17 +0530

    DbContext created

---
+++ backend/PurchaseOrderAPI/PurchaseOrderAPI/Data/PurchaseOrderContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PurchaseOrderAPI.Models;
+
+namespace PurchaseOrderAPI.Data
+{
+    public class PurchaseOrderContext : DbContext
+    {
+        public PurchaseOrderContext(DbContextOptions<PurchaseOrderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
+    }
+}
--- backend/PurchaseOrderAPI/PurchaseOrderAPI/Models/PurchaseOrder.cs
+++ backend/PurchaseOrderAPI/PurchaseOrderAPI/Models/PurchaseOrder.cs
@@ -3,7 +3,7 @@
     public class PurchaseOrder
     {
         public int Id { get; set; }
-        public string PoNumber { get; set; }  // Unique
+        public string PoNumber { get; set; }  
         public string Description { get; set; }
         public string SupplierName { get; set; }
         public DateTime OrderDate { get; set; }
