01e0c3f7bef9d90f361ff51815f1b0d11cd9b8ff
PasinduTY <pasindu.t.y.s@gmail.com>
2026-01-19 14:32:10 +0530

    created PurchaseOrdersController

---
+++ backend/PurchaseOrderAPI/PurchaseOrderAPI/Controllers/PurchaseOrdersController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PurchaseOrderAPI.Data;
+using PurchaseOrderAPI.Models;
+
+namespace PurchaseOrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchaseOrdersController : ControllerBase
+    {
+        private readonly PurchaseOrderContext _context;
+
+        public PurchaseOrdersController(PurchaseOrderContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PurchaseOrders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PurchaseOrder>>> GetPurchaseOrders()
+        {
+            return await _context.PurchaseOrders.ToListAsync();
+        }
+
+        // GET: api/PurchaseOrders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PurchaseOrder>> GetPurchaseOrder(int id)
+        {
+            var po = await _context.PurchaseOrders.FindAsync(id);
+            if (po == null)
+                return NotFound();
+
+            return po;
+        }
+
+        // POST: api/PurchaseOrders
+        [HttpPost]
+        public async Task<ActionResult<PurchaseOrder>> CreatePurchaseOrder(PurchaseOrder po)
+        {
+            //po.Status = PurchaseOrderStatus.Draft; // default
+            _context.PurchaseOrders.Add(po);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPurchaseOrder), new { id = po.Id }, po);
+        }
+
+        // PUT: api/PurchaseOrders/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePurchaseOrder(int id, PurchaseOrder po)
+        {
+            if (id != po.Id)
+                return BadRequest();
+
+            _context.Entry(po).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.PurchaseOrders.Any(e => e.Id == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/PurchaseOrders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePurchaseOrder(int id)
+        {
+            var po = await _context.PurchaseOrders.FindAsync(id);
+            if (po == null)
+                return NotFound();
+
+            _context.PurchaseOrders.Remove(po);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
